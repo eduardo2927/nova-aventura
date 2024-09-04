3add48207878e26d49141445159c532a570ff608
eduardo2927 <00001107788195sp@al.educacao.sp.gov.br>
2024-09-04 20:42:40 -0300

    projeto final

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const avanca = document.querySelectorAll(´.btn-proximo');
+
+    avanca:forEach(button => {
+        button.addEventListener('click', function(){
+            const atual document.querySelector('.ativo');
+            const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+            atual.classList.remove('ativo');
+            document.getElementById(procimoPasso).classList.add('ativo');
+        })
+    })
+
+
+
+
+
